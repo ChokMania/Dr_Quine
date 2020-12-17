d2ef282378dc4b978e8e2e08a59daec11e63a8b1
judumay <judumay@student.42.fr>
2020-12-17 12:21:18 +0100

    Sully working

--- ASM/Sully/Sully.s
+++ ASM/Sully/Sully.s
@@ -1,12 +1,12 @@
-%define ACCESS 0x2000021
+%define ACCESS 21
 section .data
 sully:
-.str db "%%define ACCESS 0x2000021%2$csection .data%2$csully:%2$c.str db %3$c%1$s%3$c,0%2$c.file db %3$cSully_%%d.s%3$c,0%2$c.exec db %3$cSully_%%d%3$c,0%2$c.w db %3$cw%3$c,0%2$c.nasm db %3$cnasm -f elf64 %%s%3$c,0%2$c.gcc db %3$cgcc %%s.o -o %%s; ./%%s%3$c,0%2$csection .text%2$cglobal main%2$cextern fprintf, fopen, fclose, sprintf, system%2$cmain:%2$cpush rbp%2$cmov rbp, rsp%2$cmov r12, %4$d%2$ccmp r12, 0%2$cjle end%2$csub rsp, 16%2$clea rdi, [rel rsp]%2$clea rsi, [rel sully.file]%2$cmov rdx, r12%2$ccall sprintf wrt ..plt%2$cmov rdi, rsp%2$cmov rsi, 0%2$cmov rax, ACCESS%2$csyscall%2$ccmp rax,0x0%2$cjnz code%2$cdec r12%2$ccode:%2$csub rsp, 16%2$clea rdi, [rel rsp]%2$clea rsi, [rel sully.file]%2$cmov rdx, r12%2$ccall sprintf wrt ..plt%2$cmov rdi, rsp%2$clea rsi, [rel sully.w]%2$ccall fopen wrt ..plt%2$cpush rax%2$cmov rdi, rax%2$cmov rax, 0%2$clea rsi, [rel sully.str]%2$clea rdx, [rel sully.str]%2$cmov rcx, 10%2$cmov r8, 34%2$cmov r9, r12%2$ccall fprintf wrt ..plt%2$cpop rdi%2$ccall fclose wrt ..plt%2$clea rdx, [rel rsp]%2$csub rsp, 16%2$clea rdi, [rel rsp]%2$clea rsi, [rel sully.nasm]%2$ccall sprintf wrt ..plt%2$clea rdi, [rel rsp]%2$ccall system wrt ..plt%2$csub rsp, 16%2$clea rdi, [rel rsp]%2$clea rsi, [rel sully.exec]%2$cmov rdx, r12%2$ccall sprintf wrt ..plt%2$clea rdx, [rel rsp]%2$csub rsp, 32%2$clea rdi, [rel rsp]%2$clea rsi, [rel sully.gcc]%2$clea rcx, [rel rsp + 32]%2$clea r8, [rel rsp + 32]%2$ccall sprintf wrt ..plt%2$clea rdi, [rel rsp]%2$ccall system wrt ..plt%2$cend:%2$cleave%2$cmov rax, 0%2$cret",0
-.file db "Sully_%d.s",0
-.exec db "Sully_%d",0
-.w db "w",0
-.nasm db "nasm -f elf64 %s",0
-.gcc db "gcc %s.o -o %s; ./%s",0
+.str db "%%define ACCESS 21%2$csection .data%2$csully:%2$c.str db %3$c%1$s%3$c, 0%2$c.file db %3$cSully_%%d.s%3$c, 0%2$c.exec db %3$cSully_%%d%3$c, 0%2$c.w db %3$cw%3$c, 0%2$c.nasm db %3$cnasm -f elf64 %%s%3$c, 0%2$c.gcc db %3$cgcc %%s.o -o %%s; ./%%s%3$c, 0%2$csection .text%2$cglobal main%2$cextern fprintf, fopen, fclose, sprintf, system%2$cmain:%2$cpush rbp%2$cmov rbp, rsp%2$cmov r12, %4$d%2$ccmp r12, 0%2$cjle end%2$csub rsp, 16%2$clea rdi, [rel rsp]%2$clea rsi, [rel sully.file]%2$cmov rdx, r12%2$ccall sprintf wrt ..plt%2$cmov rdi, rsp%2$cmov rsi, 0%2$cmov rax, ACCESS%2$csyscall%2$ccmp rax, 0x0%2$cjnz code%2$cdec r12%2$ccode:%2$csub rsp, 16%2$clea rdi, [rel rsp]%2$clea rsi, [rel sully.file]%2$cmov rdx, r12%2$ccall sprintf wrt ..plt%2$cmov rdi, rsp%2$clea rsi, [rel sully.w]%2$ccall fopen wrt ..plt%2$cpush rax%2$cmov rdi, rax%2$cmov rax, 0%2$clea rsi, [rel sully.str]%2$clea rdx, [rel sully.str]%2$cmov rcx, 10%2$cmov r8, 34%2$cmov r9, r12%2$ccall fprintf wrt ..plt%2$cpop rdi%2$ccall fclose wrt ..plt%2$clea rdx, [rel rsp]%2$csub rsp, 16%2$clea rdi, [rel rsp]%2$clea rsi, [rel sully.nasm]%2$ccall sprintf wrt ..plt%2$clea rdi, [rel rsp]%2$ccall system wrt ..plt%2$csub rsp, 16%2$clea rdi, [rel rsp]%2$clea rsi, [rel sully.exec]%2$cmov rdx, r12%2$ccall sprintf wrt ..plt%2$clea rdx, [rel rsp]%2$csub rsp, 32%2$clea rdi, [rel rsp]%2$clea rsi, [rel sully.gcc]%2$clea rcx, [rel rsp + 32]%2$clea r8, [rel rsp + 32]%2$ccall sprintf wrt ..plt%2$clea rdi, [rel rsp]%2$ccall system wrt ..plt%2$cend:%2$cleave%2$cmov rax, 0%2$cret", 0
+.file db "Sully_%d.s", 0
+.exec db "Sully_%d", 0
+.w db "w", 0
+.nasm db "nasm -f elf64 %s", 0
+.gcc db "gcc %s.o -o %s; ./%s", 0
 section .text
 global main
 extern fprintf, fopen, fclose, sprintf, system
@@ -25,7 +25,7 @@ mov rdi, rsp
 mov rsi, 0
 mov rax, ACCESS
 syscall
-cmp rax,0x0
+cmp rax, 0x0
 jnz code
 dec r12
 code:
